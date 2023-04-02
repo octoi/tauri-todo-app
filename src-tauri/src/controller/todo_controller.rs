c25ffbd64db71081fb9031cc5416cccff3d0258f
fadhilsaheer <fadhilsaheer@gmail.com>
2023-04-02 11:18:28 +0530

    feat: controller for tauri to invoke from frontend

---
+++ src-tauri/src/controller/todo_controller.rs
@@ -0,0 +1,27 @@
+use crate::database::todo_model;
+use crate::database::init_db;
+use crate::database::todo_model::TodoType;
+
+#[tauri::command]
+pub fn read_todos() -> Result<Vec<TodoType>, String> {
+    let db = init_db()?;
+    todo_model::read_all(&db)
+}
+
+#[tauri::command]
+pub fn add_todo(title: String, assigned_at: String, created_at: String) -> Result<i64, String> {
+    let db = init_db()?;
+    todo_model::create(&db, title, false, assigned_at, created_at)
+}
+
+#[tauri::command]
+pub fn update_todo(id: i32, title: String, done: bool, assigned_at: String) -> Result<(), String> {
+    let db = init_db()?;
+    todo_model::update(&db, id, title, done, assigned_at)
+}
+
+#[tauri::command]
+pub fn delete_todo(id: i32) -> Result<(), String> {
+    let db = init_db()?;
+    todo_model::delete(&db, id)
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,10 +2,18 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod database;
+mod controller;
+
+use controller::todo_controller::{add_todo, read_todos, update_todo, delete_todo};
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![
+            add_todo,
+            read_todos,
+            update_todo,
+            delete_todo
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
