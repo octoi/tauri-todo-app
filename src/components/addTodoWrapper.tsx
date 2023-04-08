ac7e7ff084013dfa331fa38ced3893bdbbc15826
Fadhil <fadhilsaheer@gmail.com>
2023-04-08 10:57:38 +0530

    feat: addTodo

--- src/components/addTodoWrapper.tsx
+++ src/components/addTodoWrapper.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import moment from 'moment';
+import * as api from '../utils/todo.api';
+import { useEffect, useState } from 'react';
 import { ReactComponent } from '../utils/react.type';
 import {
   Modal,
@@ -11,22 +13,73 @@ import {
   useDisclosure,
   Button,
   Input,
+  useToast,
 } from '@chakra-ui/react';
 
 export const AddTodoWrapper: ReactComponent = ({ children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const [title, setTitle] = useState('');
+  const [assignedAt, setAssignedAt] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const toast = useToast();
+
+  const handleAddTodo = () => {
+    if (!title) return;
+
+    setLoading(true);
+
+    api
+      .addTodo(title, moment(assignedAt).valueOf())
+      .then(() => {
+        toast({
+          title: 'Added to successfully',
+          duration: 3000,
+          isClosable: true,
+          status: 'success',
+          position: 'top-right',
+        });
+
+        onClose();
+      })
+      .catch((err) => {
+        toast({
+          title: err,
+          duration: 3000,
+          isClosable: true,
+          status: 'error',
+          position: 'top-right',
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    return () => {
+      setTitle('');
+      setAssignedAt('');
+      setLoading(false);
+    };
+  }, []);
 
   return (
     <>
       <div onClick={onOpen}>{children}</div>
 
-      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        closeOnOverlayClick={false}
+        closeOnEsc={false}
+      >
         <ModalOverlay />
         <ModalContent className='bg-app-dark1'>
           <ModalHeader>Add Todo</ModalHeader>
-          <ModalCloseButton />
+          <ModalCloseButton disabled={loading} />
           <ModalBody>
             <Input
               variant='filled'
@@ -34,12 +87,18 @@ export const AddTodoWrapper: ReactComponent = ({ children }) => {
               className='bg-app-dark2'
               value={title}
               onChange={(e) => setTitle(e.target.value)}
+              disabled={loading}
             />
             <Input
               mt={3}
               variant='filled'
-              type='date'
+              type='datetime-local'
+              value={assignedAt}
+              onChange={(e) => {
+                setAssignedAt(e.target.value);
+              }}
               className='bg-app-dark2'
+              disabled={loading}
             />
           </ModalBody>
 
@@ -48,15 +107,19 @@ export const AddTodoWrapper: ReactComponent = ({ children }) => {
               mr={2}
               onClick={onClose}
               className='bg-app-dark2 transition-all duration-200 hover:opacity-70'
+              disabled={loading}
             >
               Cancel
             </Button>
             <Button
-              className={`bg-app-accent1 transition-all duration-200 hover:opacity-80 ${
-                title.trim().length == 0 &&
-                'opacity-50 hover:opacity-50 cursor-not-allowed'
+              className={`bg-app-accent1 transition-all duration-200 ${
+                title.trim().length != 0
+                  ? 'hover:opacity-80'
+                  : 'opacity-50 hover:opacity-50 cursor-not-allowed'
               }`}
               disabled={title.trim().length == 0}
+              isLoading={loading}
+              onClick={handleAddTodo}
             >
               Add todo
             </Button>
--- src/utils/todo.api.ts
+++ src/utils/todo.api.ts
@@ -1,27 +1,27 @@
-import { invoke } from "@tauri-apps/api";
-import { TodoType } from "./todo.type";
+import { invoke } from '@tauri-apps/api';
+import { TodoType } from './todo.type';
 
-export const createTodo = (title: string, assignedAt: number) => {
-    return invoke('create_todo', {
-        title,
-        assigned_at: assignedAt.toString(),
-        created_at: Date.now().toString()
-    })
-}
+export const addTodo = (title: string, assignedAt: number) => {
+  return invoke('add_todo', {
+    title,
+    assignedAt: assignedAt.toString(),
+    createdAt: Date.now().toString(),
+  });
+};
 
 export const getTodos = () => {
-    return invoke('read_todos');
-}
+  return invoke('read_todos');
+};
 
 export const updateTodo = (todo: TodoType) => {
-    return invoke('update_todo', {
-        id: todo.id,
-        title: todo.title,
-        done: todo.done,
-        assigned_at: todo.assigned_at,
-    })
-}
+  return invoke('update_todo', {
+    id: todo.id,
+    title: todo.title,
+    done: todo.done,
+    assigned_at: todo.assigned_at,
+  });
+};
 
 export const deleteTodo = (id: number) => {
-    return invoke('delete_todo', { id })
-}
+  return invoke('delete_todo', { id });
+};
